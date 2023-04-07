64ce4527a5bcd919dc5b19ba4526269b98187226
PiotrZettt <piotrzielinski@o2.pl>
2023-04-07 20:26:25 +0100

    added argparse, error handling and parquet file

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import requests
 import os
-from dotenv import load_dotenv
+import argparse
+import json
+import logging
+import pandas as pd
 
 
-def main():
-    # User input
-    # TODO: this should be parsed from command line parameters, use argparse library for it
-    places = input("Enter the list of towns (comma-separated): ").split(",")
+def main(locations_list):
+
+    # Create Logger
+    logger = logging.getLogger(__name__)
 
     # Extract data from OpenWeatherMap API
-    # TODO: why is this dotenv library used exactly? Seems like not needed at all
-    load_dotenv()
+    # set the API key
+    try:
+        api_key = os.environ['API_KEY']
+        if not api_key:
+            raise ValueError('API_KEY environment variable is empty')
+    except KeyError:
+        raise ValueError('API_KEY environment variable is not set')
 
-    # TODO: what happens if API_KEY is not set properly? You need some kind of error detection here
-    api_key = os.getenv('API_KEY')
     url = "https://api.openweathermap.org/data/2.5/weather"
     data = []
 
-    for place in places:
-        querystring = {"q": place, "units": "metric", "appid": api_key}
-        response = requests.request("GET", url, params=querystring)
-        # TODO: we don't really use print in programs. Please use logger (library logging)
-        # this should be logged to info channel
-        print(response.status_code)
-        # TODO: what happens if this response is not a correct one, like 404 or 500?
-        # You need some kind of error detection here: response.raise_for_status()
-        weather_data = response.json()
-        data.append(weather_data)
+    # read data from 'locations_list.json' file
+    with open(locations_list, 'r') as f:
+        file_content = f.read()
+        places = json.loads(file_content)
+
+    # iterate through the locations list
+        for place in places:
+            querystring = {"q": place["name"], "units": "metric", "appid": api_key}
+
+            # send a get request
+            try:
+                response = requests.request("GET", url, params=querystring)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as error:
+                logger.error(error)
+            else:
+                logger.info(response.status_code)
+
+            weather_data = response.json()
+            data.append(weather_data)
 
     # Transform data
     formatted_data = []
@@ -36,18 +52,24 @@ def main():
         humidity = d["main"]["humidity"]
         wind_speed = d["wind"]["speed"]
         description = d["weather"][0]["description"]
-        formatted_data.append(f"The weather conditions for {place}: "
-                              f" the temperature is {temp}C, "
-                              f"humidity: {humidity}%, "
-                              f"wind: {wind_speed}m/s, "
-                              f" {description}")
+        formatted_data.append([["name", place],
+                               ["temp", str(temp)],
+                               ["humidity", str(humidity) + '%'],
+                               ["wind", str(wind_speed)],
+                               ["description", description]
+                               ])
 
     # Write data to file
-    # TODO: if we want to import it into table, this should be a machine readable format
-    # json is enough. but you could also try to save it as parquet file format as an excercise
-    with open("weather.txt", "w") as f:
-        f.write("\n".join(formatted_data))
+    with open("weather.json", "w") as f:
+        json.dump(formatted_data, f)
+
+    df = pd.DataFrame(formatted_data)
+    df.to_parquet("weather.parquet")
 
 
-with __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('locations_list')
+    args = parser.parse_args()
+    main(args.locations_list)
+
