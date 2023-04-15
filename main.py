f39ba554a4b2a0c7b19650524e01d446a74dcd62
PiotrZettt <piotrzielinski@o2.pl>
2023-04-15 08:31:06 +0100

    updated main.py

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ import argparse
 import json
 import logging
 import pandas as pd
+from datetime import datetime
 
 
 def main():
-
     # Create Logger
     logger = logging.getLogger(__name__)
-    
+
     # Add Parser
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--locations_list', nargs='+', help='A list of places to check the weather for')
+    parser.add_argument('--locations_list', nargs='+', help='A list of places to check the weather for', required=True)
     args = parser.parse_args()
     places = args.locations_list
 
     # Extract data from OpenWeatherMap API
     # set the API key
     api_key = os.environ.get('API_KEY', None)
-        api_key = os.environ['API_KEY']
-        if not api_key:
-            raise ValueError('API_KEY environment variable is empty')
-    except KeyError:
-        raise ValueError('API_KEY environment variable is not set')
 
     url = "https://api.openweathermap.org/data/2.5/weather"
     data = []
+    timestamp = datetime.now().isoformat()
 
     # iterate through the locations list
     for place in places:
@@ -49,31 +45,25 @@ def main():
         data.append(weather_data)
 
     # Transform data
-    formatted_data = []
-    json_formatted_data = []
-    for d in data:
-        place = d["name"]
-        temp = d["main"]["temp"]
-        humidity = d["main"]["humidity"]
-        wind_speed = d["wind"]["speed"]
-        description = d["weather"][0]["description"]
-        formatted_data.append([["name", place],
-                               ["temp", str(temp)],
-                               ["humidity", str(humidity) + '%'],
-                               ["wind", str(wind_speed)],
-                               ["description", description]
-                               ])
-        json_formatted_data.append({"name": place, "temp": str(temp),
-                                    "humidity": str(humidity) + '%',
-                                    "wind":str(wind_speed),
-                                    "description": description
-                                    })
+
+    json_formatted_data = [
+        {
+            "name": record["name"],
+            "temp": record["main"]["temp"],
+            "humidity": record["main"]["humidity"],
+            "wind": record["wind"]["speed"],
+            "description": record["weather"][0]["description"],
+            "extraction_timestamp": timestamp
+        }
+        for record in data
+    ]
 
     # Write data to file
     with open("weather.json", "w") as f:
-        json.dump(json_formatted_data, f)
+        for record in json_formatted_data:
+            print(json.dumps(record), file=f)
 
-    df = pd.DataFrame(formatted_data)
+    df = pd.DataFrame(json_formatted_data)
     df.to_parquet("weather.parquet")
 
 
